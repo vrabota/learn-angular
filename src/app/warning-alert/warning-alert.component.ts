8da6854fdd5df8852d77f0fb7ec4b2ebcd49acef
Veaceslav Rabota <slavic.rabota@gmail.com>
2020-05-27 23:36:24 +0300

    initial commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+})
+export class WarningAlertComponent {}
